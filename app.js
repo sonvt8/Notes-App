1bdb5be14c2babafc483fdb124ec09a3022b643c
Thai Son Vu <vuthaison198x@gmail.com>
2020-08-18 18:28:06 +0700

    Example getting Input via Command

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const command = process.argv[2]
 
 if(command === 'add'){
-    console.log('add Note')
+    console.log('add Note') //commandline: node app add  => return : add Note
+} else if(command === 'remove'){
+    console.log('remove Note') //commandline: node app remove  => return : remove Note
 }
+
