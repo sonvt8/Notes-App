7719b7d9ff31810933ed098f164572ae77371c33
Thai Son Vu <vuthaison198x@gmail.com>
2020-08-18 18:59:29 +0700

    Using Yargs to define a command

--- app.js
+++ app.js
@@ -1,8 +1,58 @@
-const command = process.argv[2]
+const chalk = require('chalk')
+const yargs = require('yargs')
+const validator = require('validator')
 
-if(command === 'add'){
-    console.log('add Note') //commandline: node app add  => return : add Note
-} else if(command === 'remove'){
-    console.log('remove Note') //commandline: node app remove  => return : remove Note
-}
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Create "Add" command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: () => console.log('Adding a new note!')
+})
+
+// Create "Remove" command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: () => console.log('Removing a note!')
+})
+
+// Create "List" command
+yargs.command({
+    command: 'list',
+    describe: 'List some notes',
+    handler: () => console.log('Listing out all notes!')
+})
+/*
+    Command: node app list
+    Return:
+    Listing out all notes!
+    { _: [ 'list' ], '$0': 'app' }
+*/
+
+// Create "Read" command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: () => console.log('Reading a note!')
+})
+/*
+    Command: node app read
+    Return:
+    Reading a note!
+    { _: [ 'read' ], '$0': 'app' }
+*/
+
+console.log(yargs.argv)
+// return: { _: [], '$0': 'app' }
+
+/*
+    For showing help, we use --help to see command funct. Ex: node app --help
+*/
+
+
+
+
 
